525739f5740f39ec4734dd139f4e233644a6fe2d
alexander <shliakhturov@gmail.com>
2024-03-23 14:26:33 +0300

    aaa

---
+++ OS/lab5-7/build/CMakeFiles/lab5-7_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab5-7_lib.dir/src/functions.cpp.o"
+  "liblab5-7_lib.a"
+  "liblab5-7_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab5-7_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ OS/lab5-7/build/CMakeFiles/worker_exe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/worker_exe.dir/src/worker.cpp.o"
+  "worker_exe"
+  "worker_exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/worker_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
